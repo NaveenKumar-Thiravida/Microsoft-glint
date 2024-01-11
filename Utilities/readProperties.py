0a6f1dd4a9558487d69c10da89d16bd2869b4019
Naveen Kumar Thiravida Selvam <naveenselvam1411@gmail.com>
2024-01-11 23:52:34 +0530

    Update

--- Utilities/readProperties.py
+++ Utilities/readProperties.py
@@ -1,25 +1,25 @@
 import configparser
 
 config=configparser.RawConfigParser()
-config.read("C:\Users\admin\PycharmProject\pythonProject\Microsoft-glint\Config\config.ini")
+config.read('C:\\Users\\admin\\PycharmProject\\pythonProject\\Microsoft-glint\\Config\\config.ini')
 
-class ReadConfig():
+class ReadConfig:
     @staticmethod
     def getApplicationURL():
-        url=config.get('Common Data','baseURL')
+        url=config.get('common','url')
         return url
 
     @staticmethod
     def getEmailAddress():
-        email = config.get('Common Data', 'Email_address')
+        email = config.get('common', 'email_address')
         return email
 
     @staticmethod
     def getClientID():
-        client = config.get('Common Data', 'Client_id')
+        client = config.get('common', 'client_id')
         return client
 
     @staticmethod
     def getPassword():
-        password = config.get('Common Data', 'Password')
+        password = config.get('common', 'password')
         return password
