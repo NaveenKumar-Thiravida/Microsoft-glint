c1b94e6a3e87a686f7edcd33347a68f943856d90
Naveen Kumar Thiravida Selvam <naveenselvam1411@gmail.com>
2024-01-13 23:29:21 +0530

    Update

--- Utilities/CustomLogger.py
+++ Utilities/CustomLogger.py
@@ -4,7 +4,7 @@ class LogGeneration:
     @staticmethod
     def loggen():
         logging.basicConfig(filename=
-                            'C:\\Users\\admin\\PycharmProject\\pythonProject\\Microsoft-glint\\Logs\\Automation.log',
+                            'C:\\Users\\admin\\PycharmProject\\pythonProject\\Microsoft-glint\\Logs',
                             format='%(asctime)s: %(levelname)s: %(message)s',datefmt='%m/%d/%Y %I:%M:%S %P')
         logger=logging.getLogger()
         logger.setLevel(logging.INFO)
