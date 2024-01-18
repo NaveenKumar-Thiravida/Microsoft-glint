b247f846abb53d85e0980bdd45e679ff003b903a
vinayakkini07 <156103852+vinayakkini07@users.noreply.github.com>
2024-01-18 19:07:11 +0530

    Delete TestCases directory

--- TestCases/conftest.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-import pytest
-
-@pytest.fixture()
-def setup():
-    driver=webdriver.Chrome(ChromeDriverManager().install())
-    return driver
